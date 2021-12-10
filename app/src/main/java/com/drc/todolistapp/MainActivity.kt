796679f2f43c3e3c4ae11ad284004c55445a13f5
nares <nareshmalviya.nitham@gmail.com>
2021-12-10 10:14:08 +0530

    project files

---
+++ app/src/main/java/com/drc/todolistapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.drc.todolistapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
